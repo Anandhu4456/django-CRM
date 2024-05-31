8ad046bee153721f524f9b572b14fa6a9ac93f6f
Anandhu4456 <anandhurajan56@gmail.com>
2024-05-31 10:43:16 +0530

    added customer model and register it in admin

--- crm/accounts/models.py
+++ crm/accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=30, null=True)
+    phone = models.CharField(max_length=10, null=True)
+    email = models.CharField(max_length=30, null=True)
+    created_date = models.DateTimeField(auto_now_add=True, null=True)
+    
+    def __str__(self) -> str:
+        return self.name
+    
+    
+        
